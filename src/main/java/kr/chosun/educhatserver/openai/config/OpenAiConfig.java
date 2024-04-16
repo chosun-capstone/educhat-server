90d57fc14d57c0168a7c40832f0d94b59e447ba0
M1NS1K <rlaalstlr2001@gmail.com>
2024-04-17 03:12:53 +0900

    fix: openai 연동 오류 해결

--- src/main/java/kr/chosun/educhatserver/openai/config/OpenAiConfig.java
+++ src/main/java/kr/chosun/educhatserver/openai/config/OpenAiConfig.java
@@ -1,8 +1,13 @@
 package kr.chosun.educhatserver.openai.config;
 
+import java.io.IOException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -13,8 +18,16 @@ public class OpenAiConfig {
     @Bean
     public RestTemplate template() {
         RestTemplate restTemplate = new RestTemplate();
+//        restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+//        restTemplate.setErrorHandler(new DefaultResponseErrorHandler() {
+//            public boolean hasError(ClientHttpResponse response) throws IOException {
+//                HttpStatus statusCode = (HttpStatus) response.getStatusCode();
+//                return statusCode.series() == HttpStatus.Series.SERVER_ERROR;
+//            }
+//        });
+
         restTemplate.getInterceptors().add(((request, body, execution) -> {
-            request.getHeaders().add("Authorization", "Bearer " + openAiKey);
+            request.getHeaders().setBearerAuth(openAiKey);
             return execution.execute(request, body);
         }));
 
--- src/main/java/kr/chosun/educhatserver/openai/controller/ChatGPTController.java
+++ src/main/java/kr/chosun/educhatserver/openai/controller/ChatGPTController.java
@@ -3,6 +3,7 @@ package kr.chosun.educhatserver.openai.controller;
 import kr.chosun.educhatserver.openai.dto.ChatGPTRequest;
 import kr.chosun.educhatserver.openai.dto.ChatGPTResponse;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api")
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class ChatGPTController {
     @Value("${openai.model}")
     private String model;
@@ -20,12 +21,13 @@ public class ChatGPTController {
     @Value("${openai.api.url}")
     private String apiURL;
 
-    private RestTemplate template;
+    private final RestTemplate template;
 
     @GetMapping("/chat")
     public String chat(@RequestParam(name = "prompt")String prompt) {
         ChatGPTRequest request = new ChatGPTRequest(model, prompt);
         ChatGPTResponse response = template.postForObject(apiURL, request, ChatGPTResponse.class);
+
         return response.getChoices().get(0).getMessage().getContent();
     }
 }
