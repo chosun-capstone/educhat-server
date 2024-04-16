7dac5ff1b0e54efb9f557bd96ff0343b9aa0d110
M1NS1K <rlaalstlr2001@gmail.com>
2024-04-16 12:15:25 +0900

    feat: ChatRecord set 및 validation 의존성 주입

---
+++ src/main/java/kr/chosun/educhatserver/openai/entity/ChatRecord.java
@@ -0,0 +1,40 @@
+package kr.chosun.educhatserver.openai.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "ChatRecord")
+public class ChatRecord {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotEmpty
+    @Column(name = "userId")
+    private String userId;
+
+    @Column(name = "userMessage")
+    private String userMessage;
+
+    @Column(name = "promptMessage")
+    private String promptMessage;
+
+    @CreatedDate
+    @Column(name = "localDateTime")
+    private LocalDateTime localDateTime;
+
+}
---
+++ src/main/java/kr/chosun/educhatserver/openai/repository/ChatRecordRepository.java
@@ -0,0 +1,6 @@
+package kr.chosun.educhatserver.openai.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//public interface ChatRecordRepository extends JpaRepository<> {
+//}
