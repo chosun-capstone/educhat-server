5c96c5272eb2f4fcb58b559cb757e66cf1d55339
M1NS1K <rlaalstlr2001@gmail.com>
2024-04-16 14:56:24 +0900

    feat: ChatRecordRepository set

--- src/main/java/kr/chosun/educhatserver/openai/repository/ChatRecordRepository.java
+++ src/main/java/kr/chosun/educhatserver/openai/repository/ChatRecordRepository.java
@@ -1,6 +1,7 @@
 package kr.chosun.educhatserver.openai.repository;
 
+import kr.chosun.educhatserver.openai.entity.ChatRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-//public interface ChatRecordRepository extends JpaRepository<> {
-//}
+public interface ChatRecordRepository extends JpaRepository<ChatRecord, Long> {g
+}
