d6c48ac905e34729e078f122531abd84618f7d28
M1NS1K <rlaalstlr2001@gmail.com>
2024-05-09 14:40:26 +0900

    feat: passwordEncoder

--- src/main/java/kr/chosun/educhatserver/security/config/SecurityConfig.java
+++ src/main/java/kr/chosun/educhatserver/security/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -31,5 +32,11 @@ public class SecurityConfig {
 						.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
 		return http.build();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+
+		return new BCryptPasswordEncoder();
 	}
 }
