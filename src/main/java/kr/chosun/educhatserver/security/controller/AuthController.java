4382bb64ee801a713a997ded046e09db6a960c72
M1NS1K <rlaalstlr2001@gmail.com>
2024-05-10 22:40:58 +0900

    feat: AuthController 구현

--- src/main/java/kr/chosun/educhatserver/security/controller/AuthController.java
+++ src/main/java/kr/chosun/educhatserver/security/controller/AuthController.java
@@ -2,7 +2,9 @@ package kr.chosun.educhatserver.security.controller;
 
 import jakarta.validation.Valid;
 import kr.chosun.educhatserver.security.dto.LoginRequestDto;
+import kr.chosun.educhatserver.security.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,10 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/auth")
 public class AuthController {
 
+	private final AuthService authService;
+
 	@PostMapping("/login")
 	public ResponseEntity<String> getUserProfile(
 			@Valid @RequestBody LoginRequestDto requestDto
 	) {
 
+		String token = authService.login(requestDto);
+		return ResponseEntity.status(HttpStatus.OK).body(token);
 	}
 }
