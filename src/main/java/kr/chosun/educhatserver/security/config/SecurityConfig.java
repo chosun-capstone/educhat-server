626f4521f0ffd0e86f33647552d2090eb76561e9
M1NS1K <rlaalstlr2001@gmail.com>
2024-05-10 21:00:16 +0900

    feat: security filter chain 구현

--- src/main/java/kr/chosun/educhatserver/security/config/SecurityConfig.java
+++ src/main/java/kr/chosun/educhatserver/security/config/SecurityConfig.java
@@ -2,34 +2,40 @@ package kr.chosun.educhatserver.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalAuthentication
 public class SecurityConfig {
+
+	private static final String[] WHITELIST = {};
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-		http
-				.csrf(AbstractHttpConfigurer::disable)
-				.formLogin(AbstractHttpConfigurer::disable)
-				.httpBasic(AbstractHttpConfigurer::disable);
+		//CSRF
+		http.csrf(AbstractHttpConfigurer::disable);
 
-		http
-				.authorizeHttpRequests((auth) -> auth
-						.requestMatchers("/login", "/", "/join").permitAll()
-						.requestMatchers("/admin").hasRole("ADMIN")
-						.anyRequest().authenticated());
+		//CORS
+		http.cors(Customizer.withDefaults());
 
-		http
-				.sessionManagement((session) -> session
-						.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		//FormLogin, BasicHttp disable
+		http.formLogin(AbstractHttpConfigurer::disable);
+		http.httpBasic(AbstractHttpConfigurer::disable);
+
+		//JwtAuthFilter를 UsernamePasswordAuthenticationFilter 앞에 추가
+
+		//권한 규칙
+		http.authorizeHttpRequests(auth -> auth
+				.requestMatchers(WHITELIST).permitAll()
+				.anyRequest().authenticated());
 
 		return http.build();
 	}
