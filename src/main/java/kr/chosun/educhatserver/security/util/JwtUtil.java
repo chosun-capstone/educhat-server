9c6a16ab4a5ea3458fd22ac561044744a73f022f
M1NS1K <rlaalstlr2001@gmail.com>
2024-05-10 21:53:31 +0900

    feat: JwtUtil 구현

--- src/main/java/kr/chosun/educhatserver/security/util/JwtUtil.java
+++ src/main/java/kr/chosun/educhatserver/security/util/JwtUtil.java
@@ -1,13 +1,16 @@
 package kr.chosun.educhatserver.security.util;
 
-import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import kr.chosun.educhatserver.security.dto.UserDto;
 import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.time.ZonedDateTime;
+import java.util.Date;
 
 @Slf4j
 @Component
@@ -20,34 +23,69 @@ public class JwtUtil {
 			@Value("${jwt.secret}") String secretKey,
 			@Value("${jwt.expiration_time}") long accessTokenExpTime
 	) {
+
 		byte[] keyBytes = Decoders.BASE64.decode(secretKey);
 		this.key = Keys.hmacShaKeyFor(keyBytes);
 		this.accessTokenExpTime = accessTokenExpTime;
 	}
 
 	//access token generate
-	public String createAccessToken() {
-		return null;
+	public String createAccessToken(UserDto user) {
+
+		return createToken(user, accessTokenExpTime);
 	}
 
 	//jwt generate
-	public String createToken() {
-		return null;
+	public String createToken(UserDto user, long expireTime) {
+
+		Claims claims = Jwts.claims();
+		claims.put("userId", user.getUserId());
+		claims.put("email", user.getEmail());
+		claims.put("role", user.getRole());
+
+		ZonedDateTime now = ZonedDateTime.now();
+		ZonedDateTime tokenValidity = now.plusSeconds(expireTime);
+
+		return Jwts.builder()
+				.setClaims(claims)
+				.setIssuedAt(Date.from(now.toInstant()))
+				.setExpiration(Date.from(tokenValidity.toInstant()))
+				.signWith(key, SignatureAlgorithm.HS256)
+				.compact();
 	}
 
 	//user id find
 	public Long getUserId(String token) {
-		return null;
+
+		return parseClaims(token).get("UserId", long.class);
 	}
 
 	//jwt valid
 	public boolean validateToken(String token) {
+
+		try {
+			Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+			return true;
+		} catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+			log.info("invalid JWT Token", e);
+		} catch (ExpiredJwtException e) {
+			log.info("Expired JWT Token", e);
+		} catch (UnsupportedJwtException e) {
+			log.info("Unsupported JWT Token", e);
+		} catch (IllegalArgumentException e) {
+			log.info("JWT claims string is empty", e);
+		}
+
 		return false;
 	}
 
 	//jwt claims
 	public Claims parseClaims(String accessToken) {
-		return null;
+		try {
+			return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(accessToken).getBody();
+		} catch (ExpiredJwtException e) {
+			return e.getClaims();
+		}
 	}
 
 }
