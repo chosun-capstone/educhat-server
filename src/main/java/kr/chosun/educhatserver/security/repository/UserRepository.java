e07eaa419133e5a0d4b306fcf306cec6a47ae35b
M1NS1K <rlaalstlr2001@gmail.com>
2024-05-10 21:31:47 +0900

    feat: UserRepository 구현

---
+++ src/main/java/kr/chosun/educhatserver/security/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package kr.chosun.educhatserver.security.repository;
+
+import kr.chosun.educhatserver.security.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findUserByEmail(String email);
+}
