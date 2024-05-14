8b8b5aa65529dc2f02067e40cf90a01804ce951d
MonaLisaSy <ugs628@naver.com>
2024-05-14 13:35:14 +0900

    center

--- 1-react/src/main.js
+++ 1-react/src/main.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from "react-dom";
-import App from "./App.js";
+import Center from './Center.js';
 
-ReactDOM.render(<App />, document.querySelector("#app"));
+ReactDOM.render(<Center />, document.querySelector("#center"));
 
 
 
