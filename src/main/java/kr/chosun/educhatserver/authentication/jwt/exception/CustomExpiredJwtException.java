91d5e7b76af6c78104c658d1f1fa03f80e361bb7
M1NS1K <rlaalstlr2001@gmail.com>
2024-05-17 16:35:17 +0900

    feat: CustomExpiredJwtException

---
+++ src/main/java/kr/chosun/educhatserver/authentication/jwt/exception/CustomExpiredJwtException.java
@@ -0,0 +1,16 @@
+package kr.chosun.educhatserver.authentication.jwt.exception;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import lombok.Getter;
+
+@Getter
+public class CustomExpiredJwtException extends ExpiredJwtException {
+
+	private final String message;
+
+	public CustomExpiredJwtException(String message, ExpiredJwtException source) {
+
+		super(source.getHeader(), source.getClaims(), source.getMessage());
+		this.message = message;
+	}
+}
